d95e36372cc2faac533d8ec63be1fcfcebbb212f
James Spicer <spicer.r.james@gmail.com>
2020-07-09 17:56:48 -0500

    working on debugging

--- src/main/java/com/teamtreehouse/stormy/MainActivity.java
+++ src/main/java/com/teamtreehouse/stormy/MainActivity.java
@@ -9,5 +9,25 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    String apiKey = "aec58531fe9e053e65d6fed3cd8f04e4";
+
+    double latitude = "37.8267";
+    double longitude = "-122.4233";
+
+
+    String forecastURL = 'https://api.darksky.net/forecast/' + apiKey + '/' + latitude + ',' + longitude;
+
+    OkHttpClient client = new OkHttpClient();
+
+    Request request = new Request.Builder()
+            .url(forecastURL)
+            .build();
+
+    Call call = client.newCall(request);
+
+    Response response = call.execute();
+
+
   }
 }
